30135f7d9b9d51d10dd1d1d6640f4e3fc74152a1
SaintsevenPark <37823692+SaintsevenPark@users.noreply.github.com>
2022-11-12 18:03:29 +0900

    first commit

---
+++ saintsevenlib/saintsevenlib.py
@@ -0,0 +1,46 @@
+import pandas_ta as pt
+import numpy as np
+
+
+def get_indicator(df):
+    bb = pt.bbands(df['Close'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+    df['BBL'] = bb[f"{bb.columns[0]}"]
+    df['BBM'] = bb[f"{bb.columns[1]}"]
+    df['BBU'] = bb[f"{bb.columns[2]}"]
+    df['BBB'] = bb[f"{bb.columns[3]}"]
+    df['BBP'] = bb[f"{bb.columns[4]}"]
+    # MACD
+    macd = pt.macd(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    df['MACD'] = macd[f"{macd.columns[0]}"]
+    df['MACDh'] = macd[f"{macd.columns[1]}"]
+    df['MACDs'] = macd[f"{macd.columns[2]}"]
+    df['MFI'] = pt.mfi(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14)
+    df['RSI'] = pt.rsi(df['Close'], timeperiod=14)
+    #
+    df['변화량'] = df['Close'] - df['Close'].shift(1)
+    df['상승폭'] = np.where(df['변화량'] >= 0, df['변화량'], 0)
+    df['하락폭'] = np.where(df['변화량'] < 0, df['변화량'].abs(), 0)
+
+    # Stochastic
+    stochastic = pt.stoch(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+    df['STOCHk'] = stochastic[f"{stochastic.columns[0]}"]
+    df['STOCHd'] = stochastic[f"{stochastic.columns[1]}"]
+
+    df['willR'] = pt.willr(df['High'], df['Low'], df['Close'], timeperiod=14)
+    df['CCI'] = pt.cci(df['High'], df['Low'], df['Close'], timeperiod=14)
+    # 단순 이동평균선
+    df['SMA5'] = pt.sma(df['Close'], 5)
+    df['SMA10'] = pt.sma(df['Close'], 10)
+    df['SMA20'] = pt.sma(df['Close'], 20)
+    df['SMA60'] = pt.sma(df['Close'], 60)
+    df['SMA120'] = pt.sma(df['Close'], 120)
+    # SuperTrend
+    supert = pt.supertrend(df['High'], df['Low'], df['Close'], length=10, multiplier=4.0)
+    df['SUPERT'] = supert[f"{supert.columns[0]}"]
+    df['SUPERTd'] = supert[f"{supert.columns[1]}"]
+    df['SUPERTl'] = supert[f"{supert.columns[2]}"]
+    df['SUPERTs'] = supert[f"{supert.columns[3]}"]
+    # CCI
+    df['CCI'] = pt.cci(df['High'], df['Low'], df['Close'], length=14)
+
+    return df
---
+++ saintsevenlib/saintsevenstrategy.py
@@ -0,0 +1,144 @@
+import numpy as np
+
+# -----------------------------   전략 1 ---------------------------------------
+# 5 이평선이 20 이평선 아래에서 V 자 반등 할고 rsi가 우상 mfi가 우상
+def strategy01(df):
+    buy, sell, superbuy, supersell = [], [], [], []
+    trigger = False
+    for i in range(2, len(df)):
+        if df['SMA20'].iloc[i] > df['SMA5'].iloc[i] > df['SMA5'].iloc[i-1] \
+            and df['SMA5'].iloc[i-1] <= df['SMA5'].iloc[i-2] < df['SMA5'].iloc[i-3] \
+            and df['RSI'].iloc[i] > df['RSI'].iloc[i-1] \
+            and df['MFI'].iloc[i] > df['MFI'].iloc[i-1]:
+            buy.append(i)
+            # if trigger == False:
+            #     buy.append(i)
+            #     trigger = True
+
+        elif df['SMA5'].iloc[i] < df['SMA20'].iloc[i] < df['SMA5'].iloc[i-1] \
+            and df['SMA5'].iloc[i-1] < df['SMA5'].iloc[i-2] \
+                and df['SMA5'].iloc[i - 2] < df['SMA5'].iloc[i-3]:
+            # and df['RSI'].iloc[i] < df['RSI'].iloc[i-1] \
+            # and df['MFI'].iloc[i] < df['MFI'].iloc[i-1]:
+
+
+            sell.append(i)
+            # if trigger == True:
+            #     sell.append(i)
+            #     trigger = False
+
+        if df.SUPERTs[i-1] > df.Close.iloc[i-1] and df.SUPERTl[i] < df.Close.iloc[i]:
+            superbuy.append(i)
+        elif df.SUPERTl[i-1] < df.Close.iloc[i-1] and df.SUPERTs[i] > df.Close.iloc[i]:
+            supersell.append(i)
+
+    return buy, sell, superbuy, supersell
+
+
+# # -----------------------------   전략 2 ---------------------------------------
+# # RSI 지표가 50을 돌파 할때 매수 50을 하락 할때 매도
+# def strategy02(df):
+#     buy, sell, superbuy, supersell = [], [], [], []
+#     trigger = False
+#     for i in range(2, len(df)):
+#         if df.RSI.iloc[i] > 50 > df.RSI.iloc[i - 1] > df.RSI.iloc[i - 2]:
+#             if trigger == False:
+#                 buy.append(i)
+#                 trigger = True
+#         elif df.RSI.iloc[i] < 50 < df.RSI.iloc[i - 1] < df.RSI.iloc[i - 2]:
+#             if trigger == True:
+#                 sell.append(i)
+#                 trigger = False
+#
+#         if df.SUPERTs[i-1] > df.close.iloc[i-1] and df.SUPERTl[i] < df.close.iloc[i]:
+#             superbuy.append(i)
+#         elif df.SUPERTl[i-1] < df.close.iloc[i-1] and df.SUPERTs[i] > df.close.iloc[i]:
+#             supersell.append(i)
+#
+#     return buy, sell, superbuy, supersell
+#
+# -----------------------------   전략 3 ---------------------------------------
+# 5 이평선이 20 이평선을 돌파 할때
+def strategy03(df):
+    buy, sell, superbuy, supersell = [], [], [], []
+    trigger = False
+    for i in range(2, len(df)):
+        if df['SMA5'].iloc[i] > df['SMA20'].iloc[i] \
+            and df['SMA5'].iloc[i-1] < df['SMA20'].iloc[i-1] \
+            and df['SMA5'].iloc[i-2] < df['SMA20'].iloc[i-2]:
+            buy.append(i)
+            # if trigger == False:
+            #     buy.append(i)
+            #     trigger = True
+        elif df['SMA5'].iloc[i] < df['SMA20'].iloc[i] \
+            and df['SMA5'].iloc[i-1] > df['SMA20'].iloc[i-1] \
+            and df['SMA5'].iloc[i-2] > df['SMA20'].iloc[i-2]:
+            sell.append(i)
+            # if trigger == True:
+            #     sell.append(i)
+            #     trigger = False
+        if df.SUPERTs[i-1] > df.Close.iloc[i-1] and df.SUPERTl[i] < df.Close.iloc[i]:
+            superbuy.append(i)
+        elif df.SUPERTl[i-1] < df.Close.iloc[i-1] and df.SUPERTs[i] > df.Close.iloc[i]:
+            supersell.append(i)
+
+    return buy, sell, superbuy, supersell
+
+
+# # -----------------------------   전략 4 ---------------------------------------
+# # # 5 이평선이 20 이평선 아래에서 V 자 반등 할때
+# # def strategy04(df):
+# #     buy, sell = [], []
+# #     for i in range(2, len(df)):
+# #         if df.MA20.iloc[i] > df.MA5.iloc[i] > df.MA5.iloc[i-1] \
+# #                 and df.MA5.iloc[i-1] <= df.MA5.iloc[i-2] \
+# #                 and df.MA5.iloc[i-2] < df.MA5.iloc[i-3]:
+# #             buy.append(i)
+# #         elif df.MA20.iloc[i] < df.MA5.iloc[i] < df.MA5.iloc[i-1] \
+# #                 and df.MA5.iloc[i-1] >= df.MA5.iloc[i-2] \
+# #                 and df.MA5.iloc[i-2] > df.MA5.iloc[i-3]:
+# #             sell.append(i)
+# #
+# #     return buy, sell
+# #
+# #
+#
+# # -----------------------------   전략 5 ---------------------------------------
+# # # 5 이평선이 20 이평선 아래에서 V 자 반등 할때 rsi가 지정수 이상 일때
+# # def strategy05(df):
+# #     buy, sell = [], []
+# #     for i in range(2, len(df)):
+# #         if df.MA20.iloc[i] > df.MA5.iloc[i] > df.MA5.iloc[i-1] \
+# #                 and df.MA5.iloc[i-1] <= df.MA5.iloc[i-2] \
+# #                 and df.MA5.iloc[i-2] < df.MA5.iloc[i-3]\
+# #                 and df.rsi.iloc[i] > df.rsi.iloc[i-1] > 30:
+# #             buy.append(i)
+# #         elif df.MA20.iloc[i] < df.MA5.iloc[i] < df.MA5.iloc[i-1] \
+# #                 and df.MA5.iloc[i-1] >= df.MA5.iloc[i-2] \
+# #                 and df.MA5.iloc[i-2] > df.MA5.iloc[i-3]:
+# #             sell.append(i)
+# #
+# #     return buy, sell
+#
+# # -----------------------------   전략 6 ---------------------------------------
+# # mfi 가 50이상에서 macd가 골든 크로스 할때
+# def strategy06(df):
+#     buy, sell, superbuy, supersell = [], [], [], []
+#     trigger = False
+#     for i in range(2, len(df)):
+#         if df.MFI.iloc[i] > df.MFI.iloc[i-1] > 50 \
+#                 and df.MACD.iloc[i] > df.MACD.iloc[i-1] \
+#                 and df.MACD.iloc[i] > df.MACDs.iloc[i] \
+#                 and df.MACD.iloc[i-1] < df.MACDs.iloc[i-1]:
+#             if trigger == False:
+#                 buy.append(i)
+#                 trigger = True
+#         elif df.MFI.iloc[i-1] > df.MFI.iloc[i] \
+#                 and df.MACD.iloc[i] < df.MACD.iloc[i-1] \
+#                 and df.MACD.iloc[i] < df.MACDs.iloc[i] \
+#                 and df.MACD.iloc[i-1] > df.MACDs.iloc[i-1]:
+#             if trigger == True:
+#                 sell.append(i)
+#                 trigger = False
+#
+#     return buy, sell, superbuy, supersell
