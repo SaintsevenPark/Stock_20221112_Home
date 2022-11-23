0397aaa242f96eb7588e78a5169363910fe917f7
SaintsevenPark <37823692+SaintsevenPark@users.noreply.github.com>
2022-11-24 00:22:25 +0900

    202211241222

--- pages/Search_Nasdaq.py
+++ pages/Search_Nasdaq.py
@@ -36,68 +36,146 @@ strategy_names_to_funcs = {
     "Strategy 6": sst.strategy06,
     "Strategy 7": sst.strategy07,
     "Strategy 8": sst.strategy08,
+    "Strategy 9": sst.strategy09,
 }
 
+df_stocklist_nasdaq = pd.read_csv('stocklist_nasdaq.csv')[0:stock_length]
+df_stocklist_nasdaq.drop(df_stocklist_nasdaq.columns[[0,3,4]], axis=1, inplace=True)
 
-df_stocklist = pd.read_csv('stocklist_nasdaq.csv')[0:stock_length]
-df_stocklist.drop([df_stocklist.columns[0], df_stocklist.columns[3], df_stocklist.columns[4]], axis=1, inplace=True)
+df_stocklist_kosdaq = pd.read_csv('stocklist_krx.csv')[0:stock_length]
+df_stocklist_kosdaq.drop(df_stocklist_kosdaq.columns[[0,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17]], axis=1, inplace=True)
 
-with st.sidebar.expander("종목목록 확장"):
-    st.dataframe(df_stocklist)
-    st.selectbox("종목선택", df_stocklist)
-    st.write(len(df_stocklist))
 
 selected_strategy = st.sidebar.selectbox("전략 선택", strategy_names_to_funcs.keys(), index=default_strategy)
 
-buy_stock_symbol = []
-buy_stock_name = []
-buy_stock_price = []
-progress_bar = st.sidebar.progress(0.0)
-# -------------------- 종목 검색
-if st.sidebar.button("코인 검색"):
-    numbers = st.sidebar.empty()
-    info = st.empty()
-    num_tick = 0
 
-    for i in range(len(df_stocklist)-1):
-        with numbers.container():
-            df = fdr.DataReader(df_stocklist['Symbol'].iloc[i], start_date)
-            df = ssl.get_indicator(df)
-            # ***********************************************************************
-            Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df)
-            st.write(desc)
-            # ***********************************************************************
-            if len(Buy) > 0:
-                # if Buy[-1] >= 198:
-                if Buy[-1] >= len(df)-2:
-                    con_text = "매수"
-                    buy_stock_symbol.append(df_stocklist['Symbol'].iloc[i])
-                    buy_stock_name.append(df_stocklist['Name'].iloc[i])
-                    buy_stock_price.append(df['Close'].iloc[-1])
+tab1, tab2 = st.tabs(['NASDAQ', 'KOSDAQ'])
+# ------------------------------ NASDAQ 종목 검색 -----------------------------------------------
+with tab1:
+    buy_stock_symbol = []
+    buy_stock_name = []
+    buy_stock_price = []
+
+    with st.expander("종목목록 확장"):
+        st.dataframe(df_stocklist_nasdaq)
+        st.selectbox("종목선택", df_stocklist_nasdaq)
+        st.write(len(df_stocklist_nasdaq))
+
+    progress_bar = st.progress(0.0)
+    if st.button("NASDAQ 에서 종목 검색"):
+        numbers = st.sidebar.empty()
+        info = st.empty()
+        num_tick = 0
+
+        for i in range(len(df_stocklist_nasdaq)-1):
+            with numbers.container():
+                df = fdr.DataReader(df_stocklist_nasdaq['Symbol'].iloc[i], start_date)
+                df = ssl.get_indicator(df)
+                # ***********************************************************************
+                if selected_strategy == 'Strategy 9':
+                    Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df, 2, -10)
+                    st.write(desc)
                 else:
-                    con_text = "매도"
-            if len(superBuy) > 0:
-                # if superBuy[-1] >= 198:
-                if superBuy[-1] >= len(df)-2:
-                    con_text = "매수"
-                    buy_stock_symbol.append(df_stocklist['Symbol'].iloc[i])
-                    buy_stock_name.append(df_stocklist['Name'].iloc[i])
-                    buy_stock_price.append(df['Close'].iloc[-1])
+                    Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df)
+                    st.write(desc)
+                # ***********************************************************************
+                if len(Buy) > 0:
+                    # if Buy[-1] >= 198:
+                    if Buy[-1] >= len(df)-2:
+                        con_text = "매수"
+                        buy_stock_symbol.append(df_stocklist_nasdaq['Symbol'].iloc[i])
+                        buy_stock_name.append(df_stocklist_nasdaq['Name'].iloc[i])
+                        buy_stock_price.append(df['Close'].iloc[-1])
+                    else:
+                        con_text = "매도"
+                if len(superBuy) > 0:
+                    # if superBuy[-1] >= 198:
+                    if superBuy[-1] >= len(df)-2:
+                        con_text = "매수"
+                        buy_stock_symbol.append(df_stocklist_nasdaq['Symbol'].iloc[i])
+                        buy_stock_name.append(df_stocklist_nasdaq['Name'].iloc[i])
+                        buy_stock_price.append(df['Close'].iloc[-1])
+                    else:
+                        con_text = "매도"
+                rtn = f"({num_tick}). {df_stocklist_nasdaq['Symbol'].iloc[i]}-{df_stocklist_nasdaq['Name'].iloc[i]}"
+                st.text(rtn)
+            num_tick = num_tick + 1
+            progress_bar.progress(int(100 * num_tick / len(df_stocklist_nasdaq)+1))
+            time.sleep(0.5)
+
+        with info.container():
+            st.sidebar.text("완료")
+        df_boughtStock_file = pd.DataFrame()
+        df_boughtStock_file['Symbol'] = buy_stock_symbol
+        df_boughtStock_file['Name'] = buy_stock_name
+        df_boughtStock_file['Price'] = buy_stock_price
+        now = datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')
+        df_boughtStock_file.to_csv(f".\\bought data\\nasdaq_{now}_{selected_strategy}.csv")
+
+
+# ------------------------------------ KOSDAQ --------------------------------------------
+with tab2:
+    buy_stock_symbol = []
+    buy_stock_name = []
+    buy_stock_price = []
+
+    with st.expander("종목목록 확장"):
+        st.dataframe(df_stocklist_kosdaq)
+        st.selectbox("종목선택", df_stocklist_kosdaq)
+        st.write(len(df_stocklist_kosdaq))
+
+    progress_bar = st.progress(0.0)
+    if st.button("KOSDAQ에서 종목 검색"):
+        numbers = st.sidebar.empty()
+        info = st.empty()
+        num_tick = 0
+
+        for i in range(len(df_stocklist_kosdaq)-1):
+            with numbers.container():
+                df = fdr.DataReader(str(df_stocklist_kosdaq['Code'].iloc[i]).zfill(6), start_date)
+                df = ssl.get_indicator(df)
+                # ***********************************************************************
+                if selected_strategy == 'Strategy 9':
+                    Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df, 2, -10)
+                    st.write(desc)
                 else:
-                    con_text = "매도"
-            rtn = f"({num_tick}). {df_stocklist['Symbol'].iloc[i]}-{df_stocklist['Name'].iloc[i]}"
-            st.text(rtn)
-        num_tick = num_tick + 1
-        progress_bar.progress(int(100 * num_tick / len(df_stocklist)+1))
-        time.sleep(0.5)
+                    Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df)
+                    st.write(desc)
+                # ***********************************************************************
+                if len(Buy) > 0:
+                    # if Buy[-1] >= 198:
+                    if Buy[-1] >= len(df)-2:
+                        con_text = "매수"
+                        buy_stock_symbol.append(df_stocklist_kosdaq['Code'].iloc[i])
+                        buy_stock_name.append(df_stocklist_kosdaq['Name'].iloc[i])
+                        buy_stock_price.append(df['Close'].iloc[-1])
+                    else:
+                        con_text = "매도"
+                if len(superBuy) > 0:
+                    # if superBuy[-1] >= 198:
+                    if superBuy[-1] >= len(df)-2:
+                        con_text = "매수"
+                        buy_stock_symbol.append(df_stocklist_kosdaq['Code'].iloc[i])
+                        buy_stock_name.append(df_stocklist_kosdaq['Name'].iloc[i])
+                        buy_stock_price.append(df['Close'].iloc[-1])
+                    else:
+                        con_text = "매도"
+                rtn = f"({num_tick}). {df_stocklist_kosdaq['Code'].iloc[i]}-{df_stocklist_kosdaq['Name'].iloc[i]}"
+                st.text(rtn)
+            num_tick = num_tick + 1
+            progress_bar.progress(int(100 * num_tick / len(df_stocklist_kosdaq)+1))
+            time.sleep(0.5)
 
-    with info.container():
-        st.sidebar.text("완료")
-    df_boughtStock_file = pd.DataFrame()
-    df_boughtStock_file['Symbol'] = buy_stock_symbol
-    df_boughtStock_file['Name'] = buy_stock_name
-    df_boughtStock_file['Price'] = buy_stock_price
-    df_boughtStock_file.to_csv(f".\\bought data\\bought_stock_nasdaq_{datetime.date.today()}_{selected_strategy}.csv")
-    st.write(datetime.datetime.now())
+        with info.container():
+            st.sidebar.text("완료")
+        df_boughtStock_file = pd.DataFrame()
+        df_boughtStock_file['Code'] = buy_stock_symbol
+        df_boughtStock_file['Name'] = buy_stock_name
+        df_boughtStock_file['Price'] = buy_stock_price
+        now = datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')
+        df_boughtStock_file.to_csv(f".\\bought data\\kosdaq_{now}_{selected_strategy}.csv")
 
 
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
