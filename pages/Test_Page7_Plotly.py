bd279b44fa26ba54051d6732807cd16065b03a43
SaintsevenPark <37823692+SaintsevenPark@users.noreply.github.com>
2022-12-11 23:45:21 +0900

    202212111145

--- pages/Test_Page7_Plotly.py
+++ pages/Test_Page7_Plotly.py
@@ -14,7 +14,6 @@ import plotly.graph_objs as go
 from saintsevenlib import saintsevenlib as ssl
 from saintsevenlib import saintsevenstrategy as sst
 
-
 l_line = 2
 s_line = -10
 stock_count = 300
@@ -62,6 +61,7 @@ with st.expander("Squeeze 데이터 프레임"):
 df = ssl.get_indicator(df_origin, l_line, s_line)
 with st.expander("모든 지표"):
     st.dataframe(df)
+
 # -----------------------------------------------------------------------
 
 stock_code = df_stocklist['Code'].iloc[number]
@@ -76,7 +76,6 @@ with st.sidebar.expander("주식 목록"):
 Buy, Sell, superBuy, superSell, desc = strategy_names_to_funcs[selected_strategy](df)
 st.write(desc)
 
-
 # MatPlotlib에서 마킹하기 위해 df 가공함
 df['Buy'] = np.nan
 for i in Buy:
@@ -86,81 +85,229 @@ df['Sell'] = np.nan
 for i in Sell:
     df['Sell'].iloc[i] = df['Close'].iloc[i]
 
+# =================== 필요한 정보만 정리해서 DataFrame 을 만듦
+df_just = pd.DataFrame()
+df_just['Open'] = df_origin['Open']
+df_just['High'] = df_origin['High']
+df_just['Low'] = df_origin['Low']
+df_just['Close'] = df_origin['Close']
+df_just['Open_HI'] = df_heiken['Open']
+df_just['High_HI'] = df_heiken['High']
+df_just['Low_HI'] = df_heiken['Low']
+df_just['Close_HI'] = df_heiken['Close']
+df_just['SUPERTl'] = df['SUPERTl']
+df_just['SUPERTs'] = df['SUPERTs']
+df_just['SUPERTlp'] = df['SUPERTlp']
+df_just['SUPERTsp'] = df['SUPERTsp']
+df_just['STOCHRSIk'] = df['STOCHRSIk']
+df_just['STOCHRSId'] = df['STOCHRSId']
+df_just['SMA5'] = df['SMA5']
+df_just['SMA20'] = df['SMA20']
+df_just['SQ_value'] = df['value']
+
+df_just['Buy'] = df['Buy']
+df_just['Sell'] = df['Sell']
+with st.expander("필요한 지표만"):
+    st.dataframe(df_just)
+# ======================================================
+
 
 # -----------------------  Plotly 시작
 # 1
-trace1_1 = go.Scatter(x = df_origin.index,
-                    y = df_origin['Close'],
-                    name='종가'
-                    )
-trace1_2 = go.Scatter(x = df_origin.index,
-                    y = df_origin['SUPERTl'],
-                    name='슈퍼트렌드 l'
-                    )
-trace1_3 = go.Scatter(x = df_origin.index,
-                    y = df_origin['SUPERTs'],
-                    name='슈퍼트렌드 s'
-                    )
-trace1_4 = go.Scatter(x = df_origin.index,
-                    y = df_origin['SUPERTlp'],
-                    name='슈퍼트렌드 l %',
-                    line=dict(width=0.5)
-                    )
-trace1_5 = go.Scatter(x = df_origin.index,
-                    y = df_origin['SUPERTsp'],
-                    name='슈퍼트렌드 s %',
-                    line=dict(width=0.5)
-                    )
+trace1_1 = go.Scatter(x=df_just.index,
+                      y=df_just['Close'],
+                      name='종가'
+                      )
+trace1_2 = go.Scatter(x=df_just.index,
+                      y=df_just['SUPERTl'],
+                      name='슈퍼트렌드 l'
+                      )
+trace1_3 = go.Scatter(x=df_origin.index,
+                      y=df_just['SUPERTs'],
+                      name='슈퍼트렌드 s'
+                      )
+trace1_4 = go.Scatter(x=df_just.index,
+                      y=df_just['SUPERTlp'],
+                      name='슈퍼트렌드 l %',
+                      line=dict(width=0.5)
+                      )
+trace1_5 = go.Scatter(x=df_just.index,
+                      y=df_just['SUPERTsp'],
+                      name='슈퍼트렌드 s %',
+                      line=dict(width=0.5)
+                      )
+trace1_6 = go.Scatter(x=df_just.index,
+                      y=df_just['Buy'],
+                      name='Buy',
+                      mode='markers',
+                      marker=dict(
+                          color='red',
+                          size=10,
+                          line=dict(
+                              color='MediumPurple',
+                              width=3
+                          ))
+                      )
 
 # 2 - Stochastic
-trace2_1 = go.Scatter(x=df_origin.index,
-                    y=df_origin['STOCHRSIk'],
-                    name='StochasticRSI K'
-                    )
-trace2_2 = go.Scatter(x=df_origin.index,
-                    y=df_origin['STOCHRSId'],
-                    name='StochasticRSI d'
-                    )
+trace2_1 = go.Scatter(x=df_just.index,
+                      y=df_just['STOCHRSIk'],
+                      name='StochasticRSI K'
+                      )
+trace2_2 = go.Scatter(x=df_just.index,
+                      y=df_just['STOCHRSId'],
+                      name='StochasticRSI d'
+                      )
+trace2_3 = go.Scatter(x=df_just.index,
+                      y=(df_just['Buy'] * 0) + df_just['STOCHRSId'],
+                      name='Buy',
+                      mode='markers',
+                      marker=dict(
+                          color='red',
+                          size=10,
+                          line=dict(
+                              color='MediumPurple',
+                              width=3
+                          ))
+                      )
 
 # 3 - 5 , 20 이동평균선
-trace3_1 = go.Scatter(x=df_origin.index,
-                    y=df['SMA5'],
-                    name='5일 이동평균선'
-                    )
-trace3_2 = go.Scatter(x=df_origin.index,
-                    y=df['SMA20'],
-                    name='20일 이동평균선'
-                    )
+trace3_1 = go.Scatter(x=df_just.index,
+                      y=df_just['SMA5'],
+                      name='5일 이동평균선'
+                      )
+trace3_2 = go.Scatter(x=df_just.index,
+                      y=df_just['SMA20'],
+                      name='20일 이동평균선'
+                      )
+trace3_3 = go.Scatter(x=df_just.index,
+                      y=df_just['Buy'] * 0.95,
+                      name='Buy',
+                      mode='markers',
+                      marker=dict(
+                          color='red',
+                          size=10,
+                          line=dict(
+                              color='MediumPurple',
+                              width=3
+                          ))
+                      )
 
-# CandleStick
-trace4_1 = go.Candlestick(x=df.index,
-                          open=df_heiken['Open'],
-                          high=df_heiken['High'],
-                          low=df_heiken['Low'],
-                          close=df_heiken['Close'],
-                          name='Candle Stick'
+# 4. Heiken Ashi
+trace4_1 = go.Candlestick(x=df_just.index,
+                          open=df_just['Open_HI'],
+                          high=df_just['High_HI'],
+                          low=df_just['Low_HI'],
+                          close=df_just['Close_HI'],
+                          name='Heiken Ashi'
                           )
+trace4_2 = go.Scatter(x=df_just.index,
+                      y=df_just['Buy'] * 1.05,
+                      name='Buy',
+                      mode='markers',
+                      marker=dict(
+                          color='red',
+                          size=10,
+                          line=dict(
+                              color='MediumPurple',
+                              width=3
+                          ))
+                      )
 
-data1 = [trace1_1, trace1_2, trace1_3, trace1_4, trace1_5]
-data2 = [trace2_1, trace2_2]
-data3 = [trace3_1, trace3_2]
-data4 = [trace4_1]
+# 5. Squeeze
+trace5_1 = go.Bar(x=df_just.index,
+                      y=df_just['SQ_value'],
+                      name='Candle Stick'
+                      )
+trace5_2 = go.Scatter(x=df_just.index,
+                      y=df_just['Buy'] * 0,
+                      name='Buy',
+                      mode='markers',
+                      marker=dict(
+                          color='red',
+                          size=10,
+                          line=dict(
+                              color='MediumPurple',
+                              width=3
+                          ))
+                      )
+# trace5_3 = go.Scatter(x=df_just.index,
+#                       y=df_just['Buy'] * 1.05,
+#                       name='Buy',
+#                       mode='markers',
+#                       marker=dict(
+#                           color='red',
+#                           size=10,
+#                           line=dict(
+#                               color='MediumPurple',
+#                               width=3
+#                           ))
+#                       )
+
+
+data1 = [trace1_1, trace1_2, trace1_3, trace1_4, trace1_5, trace1_6]
+data2 = [trace2_1, trace2_2, trace2_3]
+data3 = [trace3_1, trace3_2, trace3_3]
+data4 = [trace4_1, trace4_2]
+data5 = [trace5_1, trace5_2]
 
 fig1 = go.Figure(data=data1)
 fig2 = go.Figure(data=data2)
 fig3 = go.Figure(data=data3)
 fig4 = go.Figure(data=data4)
-fig4.update_layout(xaxis_rangeslider_visible=False)
+fig5 = go.Figure(data=data5)
 
-figs = cf.subplots([fig1, fig2, fig3, fig4], shape=(4, 1))
-figs['layout'].update(height=1600, title='PARTICLES CORRELATION')
+figs = cf.subplots([fig1, fig2, fig3], shape=(3, 1))
+figs['layout'].update(height=1500, title='PARTICLES CORRELATION')
 
-
-# for i in range(len(Buy)):
-#     fig.add_vline(x=df.iloc[Buy].index[i], line=dict(width=1, color='red', dash='dash'))
-# for i in range(len(Sell)):
-#     fig.add_vline(x=df.iloc[Sell].index[i], line=dict(width=1, color='blue', dash='dash'))
 st.plotly_chart(figs, use_container_width=True)
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+plt.style.use('fivethirtyeight')
+fig = plt.figure(figsize=(30, 15))
 
+mpf.plot(df_heiken,
+         volume_panel=2,
+         figratio=(3, 1),
+         figscale=2,
+         style='charles',
+         type='candle',
+         returnfig=True)
 
+st.pyplot()
+
+# -----------------------  Squeeze Momentum  Matplotlib 시작
+ohcl_squeeze = df_squeeze[['Open', 'High', 'Close', 'Low']]
+
+colors = []
+
+for ind, val in enumerate(df_squeeze['value']):
+    if val >= 0:
+        color = 'green'
+        if val > df_squeeze['value'][ind - 1]:
+            color = 'lime'
+    else:
+        color = 'maroon'
+        if val < df_squeeze['value'][ind - 1]:
+            color = 'red'
+    colors.append(color)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+plt.style.use('fivethirtyeight')
+fig = plt.figure(figsize=(30, 15))
+
+apds = [mpf.make_addplot(df_squeeze['value'], panel=1, type='bar', color=colors, alpha=0.5, secondary_y=False),
+        mpf.make_addplot([0] * len(df_squeeze), panel=1, type='scatter', marker='x', markersize=80,
+                         color=['green' if s else 'red' for s in df_squeeze['squeeze_off']], secondary_y=False),
+        ]
+
+mpf.plot(ohcl_squeeze,
+         volume_panel=2,
+         figratio=(3, 1),
+         figscale=1.5,
+         style='charles',
+         type='candle',
+         addplot=apds,
+         returnfig=True)
+
+st.pyplot()
